d7ba6fb4be853aaea256c7e82bc5f4365ac822c3
Mike H <michaelhanson000@gmail.com>
2024-12-23 16:50:13 -0600

    Returned urls.py file to previous version

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", https://freddiebiddle.pythonanywhere.com/admin),
-]
+    path("admin/", admin.site.urls),
+        ]
